5e91453e83daa03bd1bdd30a428f0c65c2f3f049
SerafimovichEugene <eugene.serafimovich@gmail.com>
2018-12-09 18:53:40 +0300

    wip

--- src/server/server.ts
+++ src/server/server.ts
@@ -10,7 +10,7 @@ export class Server {
     this.app = express();
     this.configureServer();
     this.configureRoutes();
-    this.configureDB();
+    // this.configureDB();
   }
 
   private configureServer(): void {
