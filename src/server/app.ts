7a97d14a2a7c30268525ae824d87da04159a172c
Serafimovich Eugene <Eugene.serafimovich@gmail.com>
2019-04-25 12:29:29 +0300

    change main project structure; applying typeorm

--- src/server/app.ts
+++ src/server/app.ts
@@ -2,19 +2,19 @@ import { Connection } from 'typeorm';
 import express, { Application } from 'express';
 import bodyParser from 'body-parser';
 import routes from './routes';
-// import { setLogger } from './configs/logger';
-// import errorHandler from './controllers/error-controller';
+import { setLogger } from './configs/logger';
+import errorHandler from './controllers/error-controller';
 // import { initPassport } from './services/auth-service';
 
 const createApp = async (connectDb: () => Promise<Connection>): Promise<Application> => {
   await connectDb();
   const app = express();
   // initPassport();
-  // app.use(setLogger);
+  app.use(setLogger);
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(routes());
-  // app.use(errorHandler);
+  app.use(errorHandler);
   return app;
 };
 
