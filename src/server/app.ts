5584f723e562996be0d4c28df50036cf1d581036
SerafimovichEugene <eugene.serafimovich@gmail.com>
2019-09-23 10:29:33 +0300

    update packages versions

--- src/server/app.ts
+++ src/server/app.ts
@@ -4,12 +4,10 @@ import bodyParser from 'body-parser';
 import routes from './routes';
 import { setLogger } from './configs/logger';
 import errorHandler from './controllers/error-controller';
-// import { initPassport } from './services/auth-service';
 
 const createApp = async (connectDb: () => Promise<Connection>): Promise<Application> => {
   await connectDb();
   const app = express();
-  // initPassport();
   app.use(setLogger);
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
--- src/server/routes.ts
+++ src/server/routes.ts
@@ -1,5 +1,4 @@
 import { Router } from 'express';
-// import { UsersController,  } from './controllers';
 
 const routes: Router = Router();
 
