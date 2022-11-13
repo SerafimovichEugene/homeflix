529f0dd002cae610639724ff3d762606923e0a47
SerafimovichEugene <Eugene.serafimovich@gmail.com>
2022-11-13 20:49:50 +0100

    update routing

--- packages/web-app/web-app-front/src/app/app.tsx
+++ packages/web-app/web-app-front/src/app/app.tsx
@@ -5,21 +5,17 @@ import {
   Routes,
   Route,
 } from 'react-router-dom';
-import Home from "../pages/Home/Home";
 import Videos from "../pages/Videos/Videos";
 import Video from "../pages/Video/Video";
-
-const HomeIndex = () => (<div>Hello</div>);
 
 const App: React.FC = () => {
   return(
     <QueryClientProvider client={new QueryClient()}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />}>
-            <Route index element={<HomeIndex />} />
-            <Route path="/videos" element={<Videos />} />
-            <Route path="/videos/:id" element={<Video />} />
+          <Route path="/">
+            <Route index element={<Videos />} />
+            <Route path="/:id" element={<Video />} />
           </Route>
         </Routes>
       </BrowserRouter>
